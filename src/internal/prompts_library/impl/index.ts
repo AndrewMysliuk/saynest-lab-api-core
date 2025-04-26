939e45f61199dd0bf327119df13702e417a4c968
Andrew Mysliuk <andre061120@gmail.com>
2025-04-26 22:31:57 +0300

    fix: modules

--- src/internal/prompts_library/impl/index.ts
+++ src/internal/prompts_library/impl/index.ts
@@ -2,7 +2,6 @@ import { IPromptService } from ".."
 import { generateFinallyPrompt, transformSingleModuleJson, transformSingleScenarioJson } from "../../..//utils"
 import CoreConversationalStructuresModule from "../../../json_module_data/core_conversational_structures.json"
 import SandboxModule from "../../../json_module_data/sandbox.json"
-import CheckingIntoHotelPrompt from "../../../json_scenario_data/checking_into_a_hotel.json"
 import LinkingIdeasClearlyPrompt from "../../../json_scenario_data/core_conversational_structures/1_linking_ideas_clearly.json"
 import StatingOpinionsComparingPrompt from "../../../json_scenario_data/core_conversational_structures/2_stating_opinions_comparing.json"
 import OrganizingYourThoughts from "../../../json_scenario_data/core_conversational_structures/3_organizing_your_thoughts.json"
@@ -10,11 +9,12 @@ import ClarifyingAndRephrasing from "../../../json_scenario_data/core_conversati
 import ExplainingCauseAndEffect from "../../../json_scenario_data/core_conversational_structures/5_explaining_cause_and_effect.json"
 import AgreeingDisagreeingReacting from "../../../json_scenario_data/core_conversational_structures/6_agreeing_disagreeing_reacting.json"
 import BuyingTimeThinkingOutLoud from "../../../json_scenario_data/core_conversational_structures/7_buying_time_thinking_out_loud.json"
-import DiscussingTechnologyAIPrompt from "../../../json_scenario_data/discussing_technology_ai.json"
-import ExplainingTechProjectSimplePrompt from "../../../json_scenario_data/explaining_tech_project_simple.json"
-import GettingGymMembershipPrompt from "../../../json_scenario_data/getting_a_gym_membership.json"
-import GettingHaircutPrompt from "../../../json_scenario_data/getting_a_haircut.json"
-import VisitingOpticianPrompt from "../../../json_scenario_data/visiting_an_optician.json"
+import CheckingIntoHotelPrompt from "../../../json_scenario_data/sandbox/checking_into_a_hotel.json"
+import DiscussingTechnologyAIPrompt from "../../../json_scenario_data/sandbox/discussing_technology_ai.json"
+import ExplainingTechProjectSimplePrompt from "../../../json_scenario_data/sandbox/explaining_tech_project_simple.json"
+import GettingGymMembershipPrompt from "../../../json_scenario_data/sandbox/getting_a_gym_membership.json"
+import TravelingAtTheAirportPrompt from "../../../json_scenario_data/sandbox/traveling_at_the_airport.json"
+import VisitingOpticianPrompt from "../../../json_scenario_data/sandbox/visiting_an_optician.json"
 import { IModuleScenario, IPromptScenario } from "../../../types"
 
 const MODULES = [
@@ -27,6 +27,7 @@ const MODULES = [
 ]
 
 const SCENARIOS = [
+  // Sandbox
   {
     ...transformSingleScenarioJson(GettingGymMembershipPrompt),
     finally_prompt: generateFinallyPrompt(GettingGymMembershipPrompt),
@@ -40,8 +41,8 @@ const SCENARIOS = [
     finally_prompt: generateFinallyPrompt(VisitingOpticianPrompt),
   },
   {
-    ...transformSingleScenarioJson(GettingHaircutPrompt),
-    finally_prompt: generateFinallyPrompt(GettingHaircutPrompt),
+    ...transformSingleScenarioJson(TravelingAtTheAirportPrompt),
+    finally_prompt: generateFinallyPrompt(TravelingAtTheAirportPrompt),
   },
   {
     ...transformSingleScenarioJson(DiscussingTechnologyAIPrompt),
